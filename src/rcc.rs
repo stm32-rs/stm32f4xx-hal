adf426882828ff5c0cf5e505958e2f068b8001e2
Aurelien Jacobs <aurel@gnuage.org>
2018-10-31 00:58:47 +0100

    rcc: export ppre1 and ppre2 in the Clocks struct

--- src/rcc.rs
+++ src/rcc.rs
@@ -88,6 +88,8 @@ impl CFGR {
                 hclk: Hertz(hclk),
                 pclk1: Hertz(hclk),
                 pclk2: Hertz(hclk),
+                ppre1: 1,
+                ppre2: 1,
                 sysclk: Hertz(sysclk),
             }
         } else if sysclk == HSI && hclk < sysclk {
@@ -120,6 +122,8 @@ impl CFGR {
                 hclk: Hertz(hclk),
                 pclk1: Hertz(hclk),
                 pclk2: Hertz(hclk),
+                ppre1: 1,
+                ppre2: 1,
                 sysclk: Hertz(sysclk),
             }
         } else {
@@ -187,8 +191,8 @@ impl CFGR {
             let ppre2 = 1 << (ppre2_bits - 0b011);
 
             // Calculate new bus clocks
-            let pclk1 = hclk / ppre1;
-            let pclk2 = hclk / ppre2;
+            let pclk1 = hclk / ppre1 as u32;
+            let pclk2 = hclk / ppre2 as u32;
 
             // Adjust flash wait states
             unsafe {
@@ -245,6 +249,8 @@ impl CFGR {
                 hclk: Hertz(hclk),
                 pclk1: Hertz(pclk1),
                 pclk2: Hertz(pclk2),
+                ppre1,
+                ppre2,
                 sysclk: Hertz(sysclk),
             }
         }
@@ -259,6 +265,8 @@ pub struct Clocks {
     hclk: Hertz,
     pclk1: Hertz,
     pclk2: Hertz,
+    ppre1: u8,
+    ppre2: u8,
     sysclk: Hertz,
 }
 
@@ -276,6 +284,16 @@ impl Clocks {
     /// Returns the frequency of the APB2
     pub fn pclk2(&self) -> Hertz {
         self.pclk2
+    }
+
+    /// Returns the prescaler of the APB1
+    pub fn ppre1(&self) -> u8 {
+        self.ppre1
+    }
+
+    /// Returns the prescaler of the APB2
+    pub fn ppre2(&self) -> u8 {
+        self.ppre2
     }
 
     /// Returns the system (core) frequency
