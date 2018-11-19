9d5235c85f47be5b7a71fecebab429cb8953008c
Aurelien Jacobs <aurel@gnuage.org>
2018-11-19 01:02:35 +0100

    rcc: add support for every stm32f4 devices

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@ pub mod gpio;
 pub mod i2c;
 #[cfg(any(feature = "stm32f401", feature = "stm32f407", feature = "stm32f412", feature = "stm32f429", feature = "stm32f411"))]
 pub mod prelude;
-#[cfg(any(feature = "stm32f401", feature = "stm32f407", feature = "stm32f412", feature = "stm32f429", feature = "stm32f411"))]
+#[cfg(any(feature = "stm32f401", feature = "stm32f405", feature = "stm32f407", feature = "stm32f410", feature = "stm32f411", feature = "stm32f412", feature = "stm32f413", feature = "stm32f415", feature = "stm32f417", feature = "stm32f423", feature = "stm32f427", feature = "stm32f429", feature = "stm32f437", feature = "stm32f439", feature = "stm32f446", feature = "stm32f469", feature = "stm32f479"))]
 pub mod rcc;
 #[cfg(any(feature = "stm32f401", feature = "stm32f407", feature = "stm32f412", feature = "stm32f429", feature = "stm32f411"))]
 pub mod serial;
