14378381b3b0310d3485dcd3b167083a3e05ce3a
Aurelien Jacobs <aurel@gnuage.org>
2019-05-01 19:07:27 +0200

    qei: set cc1s and cc2s fields directly rather than using bit shifting

--- src/qei.rs
+++ src/qei.rs
@@ -806,7 +806,7 @@ macro_rules! hal {
 
                     // Configure TxC1 and TxC2 as captures
                     tim.ccmr1_output
-                        .write(|w| unsafe { w.bits({ (0b01 << 0) | (0b01 << 8) }) });
+                        .write(|w| unsafe { w.cc1s().bits(0b01).cc2s().bits(0b01) });
 
                     // enable and configure to capture on rising edge
                     tim.ccer.write(|w| {
