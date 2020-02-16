26813a9a9f9558bf406ffc1e888312576f9b7423
Guillaume Pinot <texitoi@texitoi.eu>
2020-02-16 17:54:52 +0100

    add is_pll48clk_valid() and assert if required by the user

--- src/rcc.rs
+++ src/rcc.rs
@@ -371,7 +371,7 @@ impl CFGR {
                 })
         });
 
-        Clocks {
+        let clocks = Clocks {
             hclk: Hertz(hclk),
             pclk1: Hertz(pclk1),
             pclk2: Hertz(pclk2),
@@ -379,7 +379,13 @@ impl CFGR {
             ppre2,
             sysclk: Hertz(sysclk),
             pll48clk,
+        };
+
+        if self.pll48clk {
+            assert!(clocks.is_pll48clk_valid());
         }
+
+        clocks
     }
 }
 
@@ -431,5 +437,14 @@ impl Clocks {
     /// Returns the frequency of the PLL48 clock line
     pub fn pll48clk(&self) -> Option<Hertz> {
         self.pll48clk
+    }
+
+    /// Returns true if the PLL48 clock is within USB
+    /// specifications. It is required to use the USB functionality.
+    pub fn is_pll48clk_valid(&self) -> bool {
+        // USB specification allow +-0.25%
+        self.pll48clk
+            .map(|freq| (48_000_000 - freq.0 as i32).abs() <= 48_000_000 * 25 / 10000)
+            .unwrap_or(false)
     }
 }
