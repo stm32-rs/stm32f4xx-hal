402182bbbd2496c814b4f131384c502abca6b359
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2022-01-22 16:46:09 +0300

    fix get_max_duty

--- src/pwm.rs
+++ src/pwm.rs
@@ -72,33 +72,34 @@ macro_rules! brk {
 macro_rules! pwm_pin {
     ($TIMX:ty, $C:ty, $ccr: ident, $bit:literal) => {
         impl PwmChannel<$TIMX, $C> {
-            //NOTE(unsafe) atomic write with no side effects
             #[inline]
             pub fn disable(&mut self) {
+                //NOTE(unsafe) atomic write with no side effects
                 unsafe { bb::clear(&(*<$TIMX>::ptr()).ccer, $bit) }
             }
 
-            //NOTE(unsafe) atomic write with no side effects
             #[inline]
             pub fn enable(&mut self) {
+                //NOTE(unsafe) atomic write with no side effects
                 unsafe { bb::set(&(*<$TIMX>::ptr()).ccer, $bit) }
             }
 
-            //NOTE(unsafe) atomic read with no side effects
             #[inline]
             pub fn get_duty(&self) -> u16 {
+                //NOTE(unsafe) atomic read with no side effects
                 unsafe { (*<$TIMX>::ptr()).$ccr.read().bits() as u16 }
             }
 
-            //NOTE(unsafe) atomic read with no side effects
+            /// If `0` returned means max_duty is 2^16
             #[inline]
             pub fn get_max_duty(&self) -> u16 {
-                unsafe { (*<$TIMX>::ptr()).arr.read().bits() as u16 }
+                //NOTE(unsafe) atomic read with no side effects
+                unsafe { ((*<$TIMX>::ptr()).arr.read().bits() as u16).wrapping_add(1) }
             }
 
-            //NOTE(unsafe) atomic write with no side effects
             #[inline]
             pub fn set_duty(&mut self, duty: u16) {
+                //NOTE(unsafe) atomic write with no side effects
                 unsafe { (*<$TIMX>::ptr()).$ccr.write(|w| w.bits(duty.into())) }
             }
         }
