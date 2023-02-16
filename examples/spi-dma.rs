5045b06c70ecb1a238e0d3567f3c211f7e165912
Роман Кривенков <qwerty19106@gmail.com>
2023-02-16 15:11:30 +0400

    Add GPIO speed into SPI example

--- examples/spi-dma.rs
+++ examples/spi-dma.rs
@@ -39,8 +39,11 @@ fn main() -> ! {
         let stream = steams.4;
 
         let gpiob = dp.GPIOB.split();
-        let pb15 = gpiob.pb15.into_alternate().internal_pull_up(true);
-        let pb13 = gpiob.pb13.into_alternate();
+        
+        // Note. We set GPIO speed as VeryHigh to it corresponds to SPI frequency 3MHz.
+        // Otherwise it may lead to the 'wrong last bit in every received byte' problem.
+        let pb15 = gpiob.pb15.into_alternate().speed(Speed::VeryHigh).internal_pull_up(true);
+        let pb13 = gpiob.pb13.into_alternate().speed(Speed::VeryHigh);
 
         let mode = Mode {
             polarity: Polarity::IdleLow,
