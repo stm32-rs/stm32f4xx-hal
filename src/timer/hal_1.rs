6fa9e558e2e3f2f927f18fec3b648322acbed692
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2023-07-02 13:50:57 +0200

    implement `embedded-hal=1.0.0-alpha.10` `SetDutyCycle`

--- src/timer/hal_1.rs
+++ src/timer/hal_1.rs
@@ -3,9 +3,10 @@
 //! TIM2 and TIM5 are a general purpose 32-bit auto-reload up/downcounter with
 //! a 16-bit prescaler.
 
+use core::convert::Infallible;
 use embedded_hal_one::delay::DelayUs;
 
-use super::{Delay, Instance, SysDelay};
+use super::{Delay, Instance, PwmChannel, SysDelay, WithPwm};
 use fugit::ExtU32;
 
 impl DelayUs for SysDelay {
@@ -25,5 +26,21 @@ impl<TIM: Instance, const FREQ: u32> DelayUs for Delay<TIM, FREQ> {
 
     fn delay_ms(&mut self, ms: u32) {
         self.delay(ms.millis());
+    }
+}
+
+impl<TIM: Instance + WithPwm, const C: u8> embedded_hal_one::pwm::ErrorType for PwmChannel<TIM, C> {
+    type Error = Infallible;
+}
+
+impl<TIM: Instance + WithPwm, const C: u8> embedded_hal_one::pwm::SetDutyCycle
+    for PwmChannel<TIM, C>
+{
+    fn get_max_duty_cycle(&self) -> u16 {
+        self.get_max_duty()
+    }
+    fn set_duty_cycle(&mut self, duty: u16) -> Result<(), Self::Error> {
+        self.set_duty(duty);
+        Ok(())
     }
 }
