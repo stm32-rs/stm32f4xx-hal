72ab86bad3a4a6c2a6ad3c597e91ec7e25389d87
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2024-06-12 12:13:25 +0300

    merge f7xx-hal

--- src/timer.rs
+++ src/timer.rs
@@ -9,6 +9,7 @@ use cortex_m::peripheral::syst::SystClkSource;
 use cortex_m::peripheral::SYST;
 use enumflags2::BitFlags;
 
+#[cfg(feature = "bb")]
 use crate::bb;
 use crate::pac;
 
@@ -662,7 +663,10 @@ macro_rules! hal {
                 fn enable_channel(c: u8, b: bool) {
                     let tim = unsafe { &*<$TIM>::ptr() };
                     if c < Self::CH_NUMBER {
+                        #[cfg(feature = "bb")]
                         unsafe { bb::write(tim.ccer(), c*4, b); }
+                        #[cfg(not(feature = "bb"))]
+                        tim.ccer().modify(|_, w| w.cce(c).bit(b));
                     }
                 }
 
@@ -670,7 +674,11 @@ macro_rules! hal {
                 fn set_pwm_channel_polarity(c: u8, p: Polarity) {
                     let tim = unsafe { &*<$TIM>::ptr() };
                     if c < Self::CH_NUMBER {
-                        unsafe { bb::write(tim.ccer(), c*4 + 1, p == Polarity::ActiveLow); }
+                        let b = p == Polarity::ActiveLow;
+                        #[cfg(feature = "bb")]
+                        unsafe { bb::write(tim.ccer(), c*4 + 1, b); }
+                        #[cfg(not(feature = "bb"))]
+                        tim.ccer().modify(|_, w| w.ccp(c).bit(b));
                     }
                 }
 
@@ -678,7 +686,11 @@ macro_rules! hal {
                 fn set_pwm_nchannel_polarity(c: u8, p: Polarity) {
                     let tim = unsafe { &*<$TIM>::ptr() };
                     if c < Self::COMP_CH_NUMBER {
-                        unsafe { bb::write(tim.ccer(), c*4 + 3, p == Polarity::ActiveLow); }
+                        let b = p == Polarity::ActiveLow;
+                        #[cfg(feature = "bb")]
+                        unsafe { bb::write(tim.ccer(), c*4 + 3, b); }
+                        #[cfg(not(feature = "bb"))]
+                        tim.ccer().modify(|_, w| w.ccnp(c).bit(b));
                     }
                 }
 
@@ -717,7 +729,10 @@ macro_rules! hal {
                         let $aoe = ();
                         let tim = unsafe { &*<$TIM>::ptr() };
                         if c < Self::COMP_CH_NUMBER {
+                            #[cfg(feature = "bb")]
                             unsafe { bb::write(tim.ccer(), c*4 + 2, b); }
+                            #[cfg(not(feature = "bb"))]
+                            tim.ccer().modify(|_, w| w.ccne(c).bit(b));
                         }
                     }
                     fn set_dtg_value(value: u8) {
@@ -732,11 +747,19 @@ macro_rules! hal {
                         let tim = unsafe { &*<$TIM>::ptr() };
                         if !comp {
                             if c < Self::CH_NUMBER {
-                                unsafe { bb::write(tim.cr2(), c*2 + 8, s == IdleState::Set); }
+                                let b = s == IdleState::Set;
+                                #[cfg(feature = "bb")]
+                                unsafe { bb::write(tim.cr2(), c*2 + 8, b); }
+                                #[cfg(not(feature = "bb"))]
+                                tim.cr2().modify(|_,w| w.ois(c).bit(b));
                             }
                         } else {
                             if c < Self::COMP_CH_NUMBER {
-                                unsafe { bb::write(tim.cr2(), c*2 + 9, s == IdleState::Set); }
+                                let b = s == IdleState::Set;
+                                #[cfg(feature = "bb")]
+                                unsafe { bb::write(tim.cr2(), c*2 + 9, b); }
+                                #[cfg(not(feature = "bb"))]
+                                tim.cr2().modify(|_,w| w.oisn(c).bit(b));
                             }
                         }
                     }
