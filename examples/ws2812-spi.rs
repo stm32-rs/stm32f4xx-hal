99d0624d74f103ff23b9bd097ec8acff4d411eb9
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2026-04-17 07:53:14 +0300

    ws2812-spi 0.4

--- examples/ws2812-spi.rs
+++ examples/ws2812-spi.rs
@@ -2,6 +2,8 @@
 #![no_main]
 #![no_std]
 
+use core::array::from_fn;
+
 use panic_halt as _;
 use stm32f4xx_hal::{self as hal, rcc::Config};
 
@@ -30,19 +32,53 @@ fn main() -> ! {
         &mut rcc,
     );
 
-    const NUM_LEDS: usize = 20;
+    const NUM_LEDS: usize = 8;
     let mut buffer = [0; NUM_LEDS * 12 + 20];
     let mut ws = ws2812::prerendered::Ws2812::new(spi, buffer.as_mut_slice());
 
     // Wait before start write for syncronization
     delay.delay(200.micros());
 
+    let mut time = 0;
+
+    let ukr: [RGB8; NUM_LEDS] = from_fn(|i| {
+        if i < NUM_LEDS / 2 {
+            RGB8::new(255, 255, 0)
+        } else {
+            RGB8::new(0, 50, 255)
+        }
+    });
+    let bel: [RGB8; NUM_LEDS] = from_fn(|i| match i {
+        3..5 => RGB8::new(255, 0, 0),
+        _ => RGB8::new(255, 255, 255),
+    });
+
     loop {
-        for j in 0..(256 * 5) {
-            let data = (0..NUM_LEDS)
-                .map(|i| wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8));
-            ws.write(brightness(data, 32)).unwrap();
-            delay.delay(10.millis());
+        match time {
+            0..2 => {
+                for j in 0..(256 * 5) {
+                    let data = (0..NUM_LEDS).map(|i| {
+                        wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8)
+                    });
+                    ws.write(brightness(data, 32)).unwrap();
+                    delay.delay(10.millis());
+                }
+            }
+            2..4 => {
+                ws.write(brightness(ukr.into_iter(), 32)).unwrap();
+                delay.delay(10.millis() * (256 * 5));
+            }
+            _ => {
+                ws.write(brightness(bel.into_iter(), 32)).unwrap();
+                delay.delay(10.millis() * (256 * 5));
+            }
+        }
+        if time < 2 {
+        } else {
+        }
+        time += 1;
+        if time > 5 {
+            time = 0;
         }
     }
 }
